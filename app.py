5250630ccf1ffd1432b3ed675fc1d2f9b27d4431
donjai <n.sontichai@gmail.com>
2018-12-22 19:09:26 +0700

    Update app.py
    add save/update on redis server

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import requests
 import re
 from datetime import datetime
 import time
-async def getContent(url='',political=''):
+import redis
+
+async def getContent(url='',political='',n=0):
     #print(url)
     start = (datetime.now().timestamp())
     #print('connecting...')
@@ -12,7 +14,7 @@ async def getContent(url='',political=''):
     #print('connected')
     soup = BeautifulSoup(r.content,'html.parser')
     #asyncio.create_task(countString(soup,political))
-    await countString(soup,political)
+    await countString(soup,political,n)
     data = soup.findAll('a')
     #print('data->len',len(data))
     limit = 10
@@ -34,7 +36,7 @@ async def getContent(url='',political=''):
                     else:
                         links.append(fw_url)
                         #await asyncio.create_task(getContent(fw_url,political)) 
-                        asyncio.run(getContent(fw_url,political))
+                        asyncio.run(getContent(fw_url,political,n))
                 #fw_url = "https://www.google.com/"+fw_url
             #print(tag_a.text,'->',fw_url)
         except:
@@ -43,12 +45,14 @@ async def getContent(url='',political=''):
         
     end = (datetime.now().timestamp())  
 
-async def getGGContent(url='',political=''):
+async def getGGContent(url='',political='',n=0):
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    r.set("n"+str(n),0)
     start = (datetime.now().timestamp())
     r=requests.get(url)
     soup = BeautifulSoup(r.content,'html.parser')
     #asyncio.create_task(countString(soup,political))
-    await countString(soup,political)
+    await countString(soup,political,n)
     data = soup.findAll(attrs={"class":"r"})
     print(len(data))
     for c in data:
@@ -61,19 +65,30 @@ async def getGGContent(url='',political=''):
                 fw_url = "https://www.google.com/"+fw_url
             print(tag_a.text)
             if(fw_url.find('google') == -1):
-                await asyncio.create_task(getContent(fw_url,political)) 
+                await asyncio.create_task(getContent(fw_url,political,n)) 
                 #await asyncio.run(getContent(fw_url,political))
 
         except:
             continue
     end = (datetime.now().timestamp())  
 
-async def countString(soup,str):
+async def countString(soup,str,n):
     body = soup.findAll('body')
     cnt_str = 0
     for b in body:
         cnt_str = cnt_str + b.text.count(str)
+    await countArticle(cnt_str,n)
     print('total cnt str(',str,') = ',cnt_str)
+async def countArticle(cnt,n=0):
+    key = "n"+str(n)
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    #print(r.get(key))
+    if r.get(key) is None:
+        r.set(key,1)
+    else:
+        cnt = int(r.get(key)) + cnt
+        r.set(key,cnt)
+    #print(int(r.get("n1")))
 
 async def main():
     #total =0
@@ -81,10 +96,18 @@ async def main():
     # 10 political
     political = ['พรรคประชาธิปัตย์','พรรคประชากรไทย','พรรคมหาชน','พรรคกสิกรไทย','พรรคเพื่อฟ้าดิน','พรรคความหวังใหม่','พรรคเครือข่ายชาวนาแห่งประเทศไทย','พรรคเพื่อไทย','พรรคเพื่อแผ่นดิน','พรรคชาติพัฒนา']
     #url = 'https://en.wikipedia.org/wiki/'
+    n = 0
+    
     for p in political:
-        await getGGContent(url+p,p)
+        n = n+1
+        await getGGContent(url+p,p,n)
+    
+    #await getGGContent(url+political[0],political[0],1)
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    for key in r.keys():
+        print(key,'=',r.get(key))
     #await asyncio.create_task(getGGContent(url+political[0],political[0]))
-    #await getGGContent(url+political[0],political[0])
+    
     
 # Python 3.7+
 #total = 0
